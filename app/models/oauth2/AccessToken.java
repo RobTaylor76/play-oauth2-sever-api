2459f08d199c355848b8f50e597545cbea0fb3ee
Rob Taylor <Robert_taylor_@hotmail.com>
2014-06-24 22:18:44 +0100

    fixed deleting non existant token issue

--- app/models/oauth2/AccessToken.java
+++ app/models/oauth2/AccessToken.java
@@ -3,82 +3,83 @@ package models.oauth2;
 import javax.persistence.*;
 
 import java.sql.Timestamp;
-import java.util.Date;
+import java.util.List;
 
 import scala.Option;
 import play.db.ebean.Model;
-import scala.Some;
 
 @Entity
 @Table(name = "oauth2_access_token")
 public class AccessToken extends Model{
 
-	public static Finder<Long, AccessToken> find = new Finder<>(Long.class, AccessToken.class);
+  public static Finder<Long, AccessToken> find = new Finder<>(Long.class, AccessToken.class);
 
-	@Id
-	@Column
-	public String access_token;
+  @Id
+  @Column
+  public String access_token;
 
-	@Column
-	public String refresh_token;
+  @Column
+  public String refresh_token;
 
-	@Column 
-	public Long user_id;
-	
-	@Column
-	public String client_id;
-	
-	@Column
-	public String scope;
-	
-	@Column 
-	public Integer expires_in;
+  @Column 
+  public Long user_id;
+  
+  @Column
+  public String client_id;
+  
+  @Column
+  public String scope;
+  
+  @Column 
+  public Integer expires_in;
 
   @Column
   public Timestamp created_at;
-	
-	public AccessToken() {};
+  
+  public AccessToken() {};
 
     public AccessToken(String access_token, Option<String> refresh_token, Long user_id, Option<String> scope, int accessTokenExpiresIn, java.sql.Timestamp createdAt, String client_id) {
-    	this.access_token = access_token;
-    	if (refresh_token.nonEmpty()){
-    		this.refresh_token = refresh_token.get();
-    	}
-    	this.user_id = user_id;
-    	if (scope.nonEmpty()){
-    		this.scope = scope.get();
-    	}
-    	this.expires_in = accessTokenExpiresIn;
-    	this.created_at = createdAt;
-    	this.client_id = client_id;
+      this.access_token = access_token;
+      if (refresh_token.nonEmpty()){
+        this.refresh_token = refresh_token.get();
+      }
+      this.user_id = user_id;
+      if (scope.nonEmpty()){
+        this.scope = scope.get();
+      }
+      this.expires_in = accessTokenExpiresIn;
+      this.created_at = createdAt;
+      this.client_id = client_id;
 
     }
 
     public static AccessToken findAccessToken(String id) {
-		return find.where().eq("access_token", id).findUnique();
-	}
-	
-	public static AccessToken findToken(Long userId, String clientId) {
-		return  find.where().eq("client_id", clientId)
-				.eq("user_id", userId).findUnique();
-	}
-	
-	public static AccessToken findRefreshToken(String token) {
-		return find.where().eq("refresh_token", token).findUnique();
-		
-	}
-	
-	public static void deleteExistingAndCreate(AccessToken accessToken, 
-												Long userId,
-												String clientId) {
-		find.where().eq("client_id", clientId).eq("user_id", userId).findUnique().delete();
-		create(accessToken);
-	}
-	
-	public static AccessToken create(AccessToken token) {
-		token.save();
-		return token;
-	}
+    return find.where().eq("access_token", id).findUnique();
+  }
+  
+  public static AccessToken findToken(Long userId, String clientId) {
+    return  find.where().eq("client_id", clientId)
+        .eq("user_id", userId).findUnique();
+  }
+  
+  public static AccessToken findRefreshToken(String token) {
+    return find.where().eq("refresh_token", token).findUnique();
+  }
+  
+  public static void deleteExistingAndCreate(AccessToken accessToken, 
+                        Long userId,
+                        String clientId) {
+    List<AccessToken> tokens = find.where().eq("client_id", clientId).eq("user_id", userId).findList();
+    for(AccessToken oldToken : tokens) {
+      oldToken.delete();
+    }
+    create(accessToken);
+  }
+  
+  public static AccessToken create(AccessToken token) {
+    token.save();
+    return token;
+  }
 
 }
 /**
@@ -120,3 +121,4 @@ public class AccessToken extends Model{
   client_id varchar(80) NOT NULL,
   scope varchar(2000),
   expires_in integer NOT NULL,*/
+
